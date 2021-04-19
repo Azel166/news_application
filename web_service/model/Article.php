ea47a55843a184d90158680359e093056f25e4f3
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2021-04-19 14:49:18 +0700

    Update article-related files

--- web_service/model/Article.php
+++ web_service/model/Article.php
@@ -59,8 +59,17 @@ public function getArticlesByTag($tag_id){
     
 }
 
-public function getRelatedArticles(){
+public function getRelatedArticles($article_id){
+    $article_id = htmlspecialchars(strip_tags($article_id));
+    $query = 'SELECT * FROM '.$this->table.' INNER JOIN article_tag ON article.article_id = article_tag.article_id WHERE article_tag.tag_id IN (SELECT article_tag.tag_id FROM article INNER JOIN article_tag ON article.article_id = article_tag.article_id WHERE article.article_id = ?)';
 
+    $ps = $this->connection->prepare($query);
+
+    $ps->bindParam(1, $article_id);
+
+    $ps->execute();
+
+    return $ps;
 }
 
 // get a single article by its id
