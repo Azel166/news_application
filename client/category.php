e7cd5ccef9a3481a2a98ed224f2dfd7a120a076b
Nan <nanisme312@gmail.com>
2021-04-21 13:14:50 +0700

    nan update

---
+++ client/category.php
@@ -0,0 +1,315 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="img/favicon.png" type="image/png">
+    <title>le Nouveles - categories</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="vendors/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendors/animate-css/animate.css">
+    <link rel="stylesheet" href="vendors/popup/magnific-popup.css">
+    <!-- main css -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+</head>
+
+<body>
+
+    <!--================ Start header Top Area =================-->
+    <?php include('includes/top-header.php'); ?>
+
+    <!--================ End header top Area =================-->
+
+    <!-- Start header Menu Area -->
+    <?php include('includes/header.php'); ?>
+
+    <!-- End header Menu Area -->
+
+
+
+    <!--================Fullwidth block Area =================-->
+
+
+    <!--================Home Banner Area =================-->
+    <section class="banner_area">
+        <div class="banner_inner d-flex align-items-center">
+            <div class="container">
+                <div class="banner_content d-md-flex justify-content-between align-items-center">
+                    <div class="mb-3 mb-md-0">
+                        <h2>Category</h2>
+                    </div>
+                    <div class="page_link">
+                        <a href="index.html">Home</a>
+                        <a href="contact.html">Category</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================End Home Banner Area =================-->
+
+    <!--================Category  Area Start =================-->
+
+    <section class="category-page area-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-4">
+                    <div class="single-category">
+                        <div class="thumb">
+                            <img class="img-fluid" src="img/category/1.jpg" alt="">
+                        </div>
+                        <div class="short_details">
+                            <div class="meta-top d-flex">
+                                <a href="#">shoes </a>/
+                                <a href="#"> March 15, 2019</a>
+                            </div>
+                            <a class="d-block" href="single-blog.html">
+                                <h4>Shall for rule whose toge one
+                                    may heaven to dat</h4>
+                            </a>
+                            <div class="meta-bottom d-flex">
+                                <a href="#"><i class="ti-comment"></i>05 comment</a>
+                                <a href="#"><i class="ti-heart"></i> 0 like</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="single-category">
+                        <div class="thumb">
+                            <img class="img-fluid" src="img/category/2.jpg" alt="">
+                        </div>
+                        <div class="short_details">
+                            <div class="meta-top d-flex">
+                                <a href="#">Fashion </a>/
+                                <a href="#"> March 15, 2019</a>
+                            </div>
+                            <a class="d-block" href="single-blog.html">
+                                <h4>Whose can you're together
+                                    first dominion man</h4>
+                            </a>
+                            <div class="meta-bottom d-flex">
+                                <a href="#"><i class="ti-comment"></i>08 comment</a>
+                                <a href="#"><i class="ti-heart"></i> 0 like</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="single-category">
+                        <div class="thumb">
+                            <img class="img-fluid" src="img/category/3.jpg" alt="">
+                        </div>
+                        <div class="short_details">
+                            <div class="meta-top d-flex">
+                                <a href="#">shoes </a>/
+                                <a href="#"> March 19, 2019</a>
+                            </div>
+                            <a class="d-block" href="single-blog.html">
+                                <h4>Multiply blessed light unto
+                                    green moving</h4>
+                            </a>
+                            <div class="meta-bottom d-flex">
+                                <a href="#"><i class="ti-comment"></i>25 comment</a>
+                                <a href="#"><i class="ti-heart"></i>15 like</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="single-category">
+                        <div class="thumb">
+                            <img class="img-fluid" src="img/category/4.jpg" alt="">
+                        </div>
+                        <div class="short_details">
+                            <div class="meta-top d-flex">
+                                <a href="#">shoes </a>/
+                                <a href="#"> March 15, 2019</a>
+                            </div>
+                            <a class="d-block" href="single-blog.html">
+                                <h4>Given moved air be a male
+                                    earth called multiply</h4>
+                            </a>
+                            <div class="meta-bottom d-flex">
+                                <a href="#"><i class="ti-comment"></i>05 comment</a>
+                                <a href="#"><i class="ti-heart"></i> 10 like</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="single-category">
+                        <div class="thumb">
+                            <img class="img-fluid" src="img/category/5.jpg" alt="">
+                        </div>
+                        <div class="short_details">
+                            <div class="meta-top d-flex">
+                                <a href="#">shoes </a>/
+                                <a href="#"> March 15, 2019</a>
+                            </div>
+                            <a class="d-block" href="single-blog.html">
+                                <h4>Shall for rule whose toge one
+                                    may heaven to dat</h4>
+                            </a>
+                            <div class="meta-bottom d-flex">
+                                <a href="#"><i class="ti-comment"></i>05 comment</a>
+                                <a href="#"><i class="ti-heart"></i> 20 like</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="single-category">
+                        <div class="thumb">
+                            <img class="img-fluid" src="img/category/6.jpg" alt="">
+                        </div>
+                        <div class="short_details">
+                            <div class="meta-top d-flex">
+                                <a href="#">shoes </a>/
+                                <a href="#"> March 15, 2019</a>
+                            </div>
+                            <a class="d-block" href="single-blog.html">
+                                <h4>Given moved air be a male
+                                    earth called multiply</h4>
+                            </a>
+                            <div class="meta-bottom d-flex">
+                                <a href="#"><i class="ti-comment"></i>25 comment</a>
+                                <a href="#"><i class="ti-heart"></i> 0 like</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="single-category">
+                        <div class="thumb">
+                            <img class="img-fluid" src="img/category/7.jpg" alt="">
+                        </div>
+                        <div class="short_details">
+                            <div class="meta-top d-flex">
+                                <a href="#">shoes </a>/
+                                <a href="#"> March 15, 2019</a>
+                            </div>
+                            <a class="d-block" href="single-blog.html">
+                                <h4>Multiply blessed light unto
+                                    green moving</h4>
+                            </a>
+                            <div class="meta-bottom d-flex">
+                                <a href="#"><i class="ti-comment"></i>05 comment</a>
+                                <a href="#"><i class="ti-heart"></i> 0 like</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="single-category">
+                        <div class="thumb">
+                            <img class="img-fluid" src="img/category/8.jpg" alt="">
+                        </div>
+                        <div class="short_details">
+                            <div class="meta-top d-flex">
+                                <a href="#">shoes </a>/
+                                <a href="#"> March 15, 2019</a>
+                            </div>
+                            <a class="d-block" href="single-blog.html">
+                                <h4>Shall for rule whose toge one
+                                    may heaven to dat</h4>
+                            </a>
+                            <div class="meta-bottom d-flex">
+                                <a href="#"><i class="ti-comment"></i>05 comment</a>
+                                <a href="#"><i class="ti-heart"></i> 0 like</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4">
+                    <div class="single-category">
+                        <div class="thumb">
+                            <img class="img-fluid" src="img/category/9.jpg" alt="">
+                        </div>
+                        <div class="short_details">
+                            <div class="meta-top d-flex">
+                                <a href="#">shoes </a>/
+                                <a href="#"> March 15, 2019</a>
+                            </div>
+                            <a class="d-block" href="single-blog.html">
+                                <h4>Given moved air be a male
+                                    earth called multiply</h4>
+                            </a>
+                            <div class="meta-bottom d-flex">
+                                <a href="#"><i class="ti-comment"></i>05 comment</a>
+                                <a href="#"><i class="ti-heart"></i> 0 like</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 text-center">
+                    <a href="" class="main_btn">Load More <span class="ti-angle-double-right"></span></a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================Category  Area End =================-->
+
+    <!-- ================ Instargram Area Starts ================= -->
+
+    <section class="instagram">
+        <div class="row no-gutters">
+            <div class="col-2">
+                <a href=""><img src="img/instagram/1.jpg" alt=""></a>
+            </div>
+            <div class="col-2">
+                <a href=""><img src="img/instagram/2.jpg" alt=""></a>
+            </div>
+            <div class="col-2">
+                <a href=""><img src="img/instagram/3.jpg" alt=""></a>
+            </div>
+            <div class="col-2">
+                <a href=""><img src="img/instagram/4.jpg" alt=""></a>
+            </div>
+            <div class="col-2">
+                <a href=""><img src="img/instagram/5.jpg" alt=""></a>
+            </div>
+            <div class="col-2">
+                <a href=""><img src="img/instagram/6.jpg" alt=""></a>
+            </div>
+        </div>
+    </section>
+
+    <!-- ================ Instargram Area End ================= -->
+
+
+    <!-- ================ start footer Area ================= -->
+    <?php include('includes/footer.php'); ?>
+
+    <!-- ================ End footer Area ================= -->
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/stellar.js"></script>
+    <script src="vendors/popup/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/mail-script.js"></script>
+    <script src="js/contact.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+    <script src="js/theme.js"></script>
+</body>
+
+</html>
---
+++ client/contact.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="img/favicon.png" type="image/png">
+    <title>le Nouveles</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="vendors/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendors/animate-css/animate.css">
+    <!-- main css -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+</head>
+<body>
+
+
+    <!--================ Start header Top Area =================-->
+     <!--================ Start header Top Area =================-->
+     <?php include('includes/top-header.php');?>
+
+     <!--================ End header top Area =================-->
+ 
+     <!-- Start header Menu Area -->
+     <?php include('includes/header.php');?>
+ 
+     <!-- End header Menu Area -->
+ 
+
+    
+            </div>
+            <div class="form-group mt-3">
+              <button type="submit" class="button button-contactForm">Send Message</button>
+            </div>
+          </form>
+
+
+        </div>
+
+        <div class="col-lg-4">
+          <div class="media contact-info">
+            <span class="contact-info__icon"><i class="ti-home"></i></span>
+            <div class="media-body">
+              <h3>Buttonwood, California.</h3>
+              <p>Rosemead, CA 91770</p>
+            </div>
+          </div>
+          <div class="media contact-info">
+            <span class="contact-info__icon"><i class="ti-tablet"></i></span>
+            <div class="media-body">
+              <h3><a href="tel:454545654">00 (440) 9865 562</a></h3>
+              <p>Mon to Fri 9am to 6pm</p>
+            </div>
+          </div>
+          <div class="media contact-info">
+            <span class="contact-info__icon"><i class="ti-email"></i></span>
+            <div class="media-body">
+              <h3><a href="mailto:support@colorlib.com">support@colorlib.com</a></h3>
+              <p>Send us your query anytime!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- ================ contact section end ================= -->
+
+
+
+
+    <!-- ================ Instargram Area Starts ================= -->  
+
+    <section class="instagram">
+        <div class="row no-gutters">
+            <div class="col-2">
+                <a href=""><img src="img/instagram/1.jpg" alt=""></a>
+            </div>
+            <div class="col-2">
+                <a href=""><img src="img/instagram/2.jpg" alt=""></a>
+            </div>
+            <div class="col-2">
+                <a href=""><img src="img/instagram/3.jpg" alt=""></a>
+            </div>
+            <div class="col-2">
+                <a href=""><img src="img/instagram/4.jpg" alt=""></a>
+            </div>
+            <div class="col-2">
+                <a href=""><img src="img/instagram/5.jpg" alt=""></a>
+            </div>
+            <div class="col-2">
+                <a href=""><img src="img/instagram/6.jpg" alt=""></a>
+            </div>
+        </div>
+    </section>
+
+    <!-- ================ Instargram Area End ================= -->  
+
+
+    <!-- ================ start footer Area ================= -->
+    <?php include('includes/footer.php');?>
+
+<!-- ================ End footer Area ================= -->
+
+
+<!--================Contact Success and Error message Area =================-->
+    <div id="success" class="modal modal-message fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <i class="fas fa-times"></i>
+                    </button>
+                    <h2>Thank you</h2>
+                    <p>Your message is successfully sent...</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modals error -->
+
+    <div id="error" class="modal modal-message fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <i class="fas fa-times"></i>
+                    </button>
+                    <h2>Sorry !</h2>
+                    <p> Something went wrong </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--================End Contact Success and Error message Area =================-->
+
+
+
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="js/jquery-2.2.4.min.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/stellar.js"></script>
+<script src="vendors/popup/jquery.magnific-popup.min.js"></script>
+<script src="js/jquery.ajaxchimp.min.js"></script>
+<script src="js/waypoints.min.js"></script>
+<script src="js/mail-script.js"></script>
+<script src="js/contact.js"></script>
+<script src="js/jquery.form.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+<script src="js/mail-script.js"></script>
+<script src="js/theme.js"></script>
+</body>
+</html>
