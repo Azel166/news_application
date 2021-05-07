fea5d352bae270eb9545316f1cb8e421c5e60ec6
Nan <nanisme312@gmail.com>
2021-05-08 02:32:26 +0700

    image to cate & article

--- client/includes/related_post.php
+++ client/includes/related_post.php
@@ -2,10 +2,10 @@
 $article_array = array();
 foreach ($result as $key => $value) :
     $article_id = $value['article_id'];
-    $urlR = "http://localhost:8080/news_application/web_service/api/article/get_related_articles.php?id=$article_id";
-    $article = curl_init($urlR);
-    curl_setopt($article, CURLOPT_RETURNTRANSFER, true);
-    $responseR = curl_exec($article);
+    $relatedUrl = "http://localhost:8080/news_application/web_service/api/article/get_related_articles.php?id=$article_id";
+    $articleRelated = curl_init($relatedUrl);
+    curl_setopt($articleRelated, CURLOPT_RETURNTRANSFER, true);
+    $responseR = curl_exec($articleRelated);
     $resultR = json_decode($responseR, true);
     $article_array = array_merge($article_array, $resultR);
 endforeach;
@@ -15,18 +15,25 @@ $i = 0;
 <aside class="single_sidebar_widget popular_post_widget">
     <h3 class="widget_title">Recent Post</h3>
 
-    <?php foreach ($article_array as $keyA => $valueA) :?>
-            <div class="media post_item">
-                <img src="img/blog/popular-post/post1.jpg" alt="post">
-                <div class="media-body">
-                    <a href="#">
-                        <h3><?php echo $value['title']; ?>.</h3>
-                    </a>
-                    <p>January 12, 2019</p>
-                </div>
+    <?php foreach ($article_array as $keyA => $valueA) : ?>
+        <div class="media post_item">
+            <img src="img/blog/popular-post/post1.jpg" alt="post">
+            <div class="media-body">
+                <a href="#">
+                    <h3><?php echo $value['title']; ?>.</h3>
+                </a>
+                <?php $date = strtotime($value['date_created']); ?>
+                <?php
+                $day = date('d', $date);
+                $mon_num = date('m', $date);
+                $mon = date("F", mktime(0, 0, 0, $mon_num, 10));
+                $yr = date('Y', $date);
+                ?>
+                <p> <?php echo $mon  ?> <?php echo  $day ?>  <?php echo $yr  ?> </p>
             </div>
+        </div>
 
-        <?php if (++$i == 4) break;?>
+        <?php if (++$i == 4) break; ?>
 
     <?php endforeach; ?>
 </aside>
