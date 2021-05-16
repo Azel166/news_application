0a2f67e8631705cc5bedec85b330d07d16c6869d
Nan <nanisme312@gmail.com>
2021-05-17 06:48:36 +0700

    image update

--- client/index.php
+++ client/index.php
@@ -135,7 +135,7 @@ $i = 0;
 
             </div>
             <div class="row">
-                <div class="col-lg-7">
+                <div class="col-lg-8">
                     <div class="single-blog video-style">
                         <div class="thumb">
                         <img class="img-fluid" src="img/articles/<?php echo $result[15]['image']; ?>" alt="">
@@ -159,13 +159,11 @@ $i = 0;
 
                 </div> 
 
-                <div class="col-lg-5">
+                <div class="col-lg-4">
                     <div class="single-blog video-style small row m_b_30">
-                        <div class="thumb col-12 col-sm-5">
-                        <img class="img-fluid" src="img/articles/<?php echo $result[20]['image']; ?>" alt="">
-    
-                        </div>
-                        <div class="short_details col-12 col-sm-7">
+                        <div class="short_details ">
+                        <img class="img-fluid " src="img/articles/<?php echo $result[20]['image']; ?>" alt="">
+
                             <div class="meta-top d-flex">
                                 <a href="#">Beauty</a>
                             </div>
@@ -182,11 +180,8 @@ $i = 0;
                     </div> 
 
                     <div class="single-blog video-style small row m_b_30 ">
-                        <div class="thumb col-12 col-sm-5">
-                        <img class="img-fluid" src="img/articles/<?php echo $result[23]['image']; ?>" alt="">
-                            
-                        </div>
-                        <div class="short_details col-12 col-sm-7">
+                        
+                        <div class="short_details col-12 col-sm-12">
                             <div class="meta-top d-flex">
                                 <a href="#">Beauty</a>
                             </div>
@@ -201,31 +196,6 @@ $i = 0;
                             </div>
                         </div>
                     </div>
-
-                    <div class="single-blog video-style small row">
-                        <div class="thumb col-12 col-sm-5">
-                            <img class="img-fluid" src="img/magazine/14.jpg" alt="">
-                            <div class="play_btn">
-                                <a class="play-video" href="https://www.youtube.com/watch?v=MrRvX5I8PyY" data-animate="zoomIn"
-                                data-duration="1.5s" data-delay="0.1s"><span class="ti-control-play"></span></a>
-                            </div>
-                        </div>
-                        <div class="short_details col-12 col-sm-7">
-                            <div class="meta-top d-flex">
-                                <a href="#">Beauty</a>
-                            </div>
-                            <a class="d-block" href="single-blog.html">
-                                <h4>Blessed night morning on
-                                them you great</h4>
-                            </a>
-                            <div class="meta-bottom d-flex">
-                                <a href="#"><i class="ti-time"></i>mar 12</a>
-                                <a href="#"><i class="ti-heart"></i> 0 like</a>
-                                <a href="#"><i class="ti-eye"></i> 1k view</a>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
             </div>
         </div>
@@ -241,8 +211,8 @@ $i = 0;
         <div class="container">
             <div class="row">
                 <div class="area-heading">
-                    <h3>Fashion News</h3>
-                    <p>Abundantly creeping saw forth spirit can made appear fourth us.</p>
+                    <h3>Style News</h3>
+                    <p>Brings you the latest coverage on celebrity and entertainment news, beauty and style trends, women's health information and more.</p>
                 </div>
 
             </div>
@@ -250,7 +220,7 @@ $i = 0;
                 <div class="col-lg-4">
                     <div class="single-blog style-five">
                         <div class="thumb">
-                            <img class="img-fluid" src="img/magazine/15.jpg" alt="">
+                        <img class="img-fluid " src="img/<?php echo $result[21]['image']; ?>" alt="">
                         </div>
                         <div class="short_details">
                             <div class="meta-top d-flex">
@@ -273,7 +243,7 @@ $i = 0;
                 <div class="col-lg-4">
                     <div class="single-blog style-five">
                         <div class="thumb">
-                            <img class="img-fluid" src="img/magazine/16.jpg" alt="">
+                        <img class="img-fluid " src="img/articles/<?php echo $result[25]['image']; ?>" alt="">
                         </div>
                         <div class="short_details">
                             <div class="meta-top d-flex">
@@ -296,7 +266,7 @@ $i = 0;
                 <div class="col-lg-4">
                     <div class="single-blog style-five">
                         <div class="thumb">
-                            <img class="img-fluid" src="img/magazine/17.jpg" alt="">
+                        <img class="img-fluid " src="img/<?php echo $result[26]['image']; ?>" alt="">
                         </div>
                         <div class="short_details">
                             <div class="meta-top d-flex">
