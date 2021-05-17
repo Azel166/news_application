bfbf549193fd3fe25e567178e122c21667a5cfcf
Nan <nanisme312@gmail.com>
2021-05-17 08:16:05 +0700

    update contact & resources

--- client/contact.php
+++ client/contact.php
@@ -30,9 +30,37 @@
      <?php include('includes/header.php');?>
  
      <!-- End header Menu Area -->
- 
+     <section class="contact-section area-padding">
+     <div class="container">
 
-    
+
+     <div class="row">
+        <div class="col-12">
+          <h2 class="contact-title">Get in Touch</h2>
+        </div>
+        <div class="col-lg-8">
+          <form class="form-contact contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
+            <div class="row">
+              <div class="col-12">
+                <div class="form-group">
+                    <textarea class="form-control w-100" name="message" id="message" cols="30" rows="9" placeholder="Enter Message"></textarea>
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="form-group">
+                  <input class="form-control" name="name" id="name" type="text" placeholder="Enter your name">
+                </div>
+              </div>
+              <div class="col-sm-6">
+                <div class="form-group">
+                  <input class="form-control" name="email" id="email" type="email" placeholder="Enter email address">
+                </div>
+              </div>
+              <div class="col-12">
+                <div class="form-group">
+                  <input class="form-control" name="subject" id="subject" type="text" placeholder="Enter Subject">
+                </div>
+              </div>
             </div>
             <div class="form-group mt-3">
               <button type="submit" class="button button-contactForm">Send Message</button>
@@ -46,21 +74,21 @@
           <div class="media contact-info">
             <span class="contact-info__icon"><i class="ti-home"></i></span>
             <div class="media-body">
-              <h3>Buttonwood, California.</h3>
-              <p>Rosemead, CA 91770</p>
+              <h3>Hanoi University</h3>
+              <p>Km9, nguyen trai, thanh xuan, ha noi.</p>
             </div>
           </div>
           <div class="media contact-info">
             <span class="contact-info__icon"><i class="ti-tablet"></i></span>
             <div class="media-body">
-              <h3><a href="tel:454545654">00 (440) 9865 562</a></h3>
-              <p>Mon to Fri 9am to 6pm</p>
+              <h3><a href="tel:1234567890">1234567890</a></h3>
+              <p>Mon to Fri 8am to 5pm</p>
             </div>
           </div>
           <div class="media contact-info">
             <span class="contact-info__icon"><i class="ti-email"></i></span>
             <div class="media-body">
-              <h3><a href="mailto:support@colorlib.com">support@colorlib.com</a></h3>
+              <h3><a href="mailto:support@colorlib.com">lenovelles@s.hanu.edu.vn</a></h3>
               <p>Send us your query anytime!</p>
             </div>
           </div>
@@ -69,73 +97,6 @@
     </div>
   </section>
   <!-- ================ contact section end ================= -->
-
-
-
-
-    <!-- ================ Instargram Area Starts ================= -->  
-
-    <section class="instagram">
-        <div class="row no-gutters">
-            <div class="col-2">
-                <a href=""><img src="img/instagram/1.jpg" alt=""></a>
-            </div>
-            <div class="col-2">
-                <a href=""><img src="img/instagram/2.jpg" alt=""></a>
-            </div>
-            <div class="col-2">
-                <a href=""><img src="img/instagram/3.jpg" alt=""></a>
-            </div>
-            <div class="col-2">
-                <a href=""><img src="img/instagram/4.jpg" alt=""></a>
-            </div>
-            <div class="col-2">
-                <a href=""><img src="img/instagram/5.jpg" alt=""></a>
-            </div>
-            <div class="col-2">
-                <a href=""><img src="img/instagram/6.jpg" alt=""></a>
-            </div>
-        </div>
-    </section>
-
-    <!-- ================ Instargram Area End ================= -->  
-
-
-    <!-- ================ start footer Area ================= -->
-    <?php include('includes/footer.php');?>
-
-<!-- ================ End footer Area ================= -->
-
-
-<!--================Contact Success and Error message Area =================-->
-    <div id="success" class="modal modal-message fade" role="dialog">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <i class="fas fa-times"></i>
-                    </button>
-                    <h2>Thank you</h2>
-                    <p>Your message is successfully sent...</p>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Modals error -->
-
-    <div id="error" class="modal modal-message fade" role="dialog">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <i class="fas fa-times"></i>
-                    </button>
-                    <h2>Sorry !</h2>
-                    <p> Something went wrong </p>
-                </div>
-            </div>
-        </div>
     </div>
     <!--================End Contact Success and Error message Area =================-->
 
